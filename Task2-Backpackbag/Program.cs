e8ac779c3c156530d997b354eda947c52ffd00a1
henrikdevops <henrik.jansson19872@gmail.com>
2025-06-04 12:10:33 +0200

    Add project files.

---
+++ Task2-Backpackbag/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace Task2_Backpackbag
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool minBool = true;
+            string item = "";
+
+            while (minBool)
+            {
+                Console.WriteLine("Välkommen till Ryggsäcken");
+                Console.WriteLine("[1] - Lägg till ett föremål:");
+                Console.WriteLine("[2] - Skriv ut det tillagda föremålet:");
+                Console.WriteLine("[3] - Rensa ryggsäcken:");
+                Console.WriteLine("[4] - Avsluta programmet:");
+
+                int input = Convert.ToInt32(Console.ReadLine());
+
+                if (input == 1)
+                {
+                    Console.Write("Skriv ett föremål du vill lägga till i ryggsäcken:");
+                    item = Console.ReadLine();
+                }
+                else if (input == 2)
+                {
+                    Console.WriteLine("Du har lagt till:" + item);
+                }
+                else if (input == 3)
+                {
+                    //Menyval 3
+                }
+                else if (input == 4)
+                {
+                    minBool = false;
+                    Console.WriteLine("Programmet avslutas...");
+                    Console.ReadLine();
+                }
+            }
+        }
+    }
+}

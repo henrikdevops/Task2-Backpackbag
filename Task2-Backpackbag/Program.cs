e9bbf5e11506bfab24a59386a9f06f6b5db584f8
henrikdevops <henrik.jansson19872@gmail.com>
2025-06-04 12:14:57 +0200

    Update Program.cs

--- Task2-Backpackbag/Program.cs
+++ Task2-Backpackbag/Program.cs
@@ -1,38 +1,58 @@
-﻿namespace Task2_Backpackbag
+namespace Task2_Backpackbag
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            // Skapa en meny som låter användaren lägga till ett föremål i ryggsäcken, skriva ut det tillagda föremålet och rensa ryggsäcken.
             bool minBool = true;
+
+            // Skapa en variabel som lagrar det tillagda föremålet.
             string item = "";
 
+            // Skapa en meny som låter användaren välja mellan att lägga till ett föremål, skriva ut det tillagda föremålet, rensa ryggsäcken eller avsluta programmet.
             while (minBool)
             {
+                // Skriver ut Menyval
                 Console.WriteLine("Välkommen till Ryggsäcken");
                 Console.WriteLine("[1] - Lägg till ett föremål:");
                 Console.WriteLine("[2] - Skriv ut det tillagda föremålet:");
                 Console.WriteLine("[3] - Rensa ryggsäcken:");
                 Console.WriteLine("[4] - Avsluta programmet:");
 
+                // Läs in användarens val
                 int input = Convert.ToInt32(Console.ReadLine());
 
+                // Hantera användarens val
                 if (input == 1)
                 {
                     Console.Write("Skriv ett föremål du vill lägga till i ryggsäcken:");
                     item = Console.ReadLine();
                 }
+                // Om användaren väljer 2, skriv ut det tillagda föremålet
                 else if (input == 2)
                 {
                     Console.WriteLine("Du har lagt till:" + item);
                 }
+                // Om användaren väljer 3, rensa ryggsäcken
                 else if (input == 3)
                 {
-                    //Menyval 3
+                    // Rensa ryggsäcken genom att sätta item till en tom sträng
+                    item = ""; 
+                    Console.WriteLine("Ryggsäcken har rensats.");
                 }
+                else if (input == 3)
+                {
+                    // item = ""; // Rensa ryggsäcken genom att sätta item till en tom sträng
+                    item )= ""; 
+                }
+                // Om användaren väljer 4, avsluta programmet
                 else if (input == 4)
                 {
+                    // Sätt minBool till false för att avsluta loopen
                     minBool = false;
+
+                    // Skriv ut ett meddelande om att programmet avslutas
                     Console.WriteLine("Programmet avslutas...");
                     Console.ReadLine();
                 }
